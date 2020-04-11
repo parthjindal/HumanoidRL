7520d25b470603aa1d3ace75dbc817e54d291efc
Sry-421 <poddarsriyash@gmail.com>
2020-04-11 11:43:00 +0530

    test bug resolved

--- test_policy.py
+++ test_policy.py
@@ -1,19 +1,32 @@
-from spinup.utils.test_policy import load_policy, run_policy
+from spinup.utils.test_policy import load_pytorch_policy, load_tf_policy, run_policy
+from spinup.utils.plot import make_plots
 import HumanoidRL
 import gym
 import argparse
 
-def main() :
-    
+
+if __name__ == '__main__' :
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--file",
         type=str,
         default="",
     )
+    parser.add_argument(
+        "--plot",
+        type=bool,
+        default=False,
+    )
+    parser.add_argument(
+        "--run",
+        type=str,
+        default=None,
+        choices={"pytorch", "tf"}
+    )
     args = parser.parse_args()
-    _, get_action = load_policy(args.file)
-    env = gym.make('HumanoidRL-v0')
-    run_policy(env, get_action)
-
-main()
+    if args.plot:
+        make_plots([args.file], xaxis="TotalEnvInteracts", values="Performance")
+    if args.run:
+        get_action = load_pytorch_policy(args.file, itr='') if args.run == "pytorch" else load_tf_policy(args.file, itr='')
+        env = gym.make('HumanoidRL-v0')
+        run_policy(env, get_action)
