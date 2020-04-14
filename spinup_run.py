6dc62bf7be6d3bc3cb740b31cc1c32ae327c6a5a
Sry-421 <poddarsriyash@gmail.com>
2020-04-14 20:16:48 +0530

    updated run file

--- spinup_run.py
+++ spinup_run.py
@@ -1,7 +1,7 @@
 from spinup.utils.run_utils import ExperimentGrid
-from spinup import ppo_tf1
+from spinup import ppo_pytorch
 import gym
-import tensorflow as tf
+import torch
 
 def run_experiment(args):
     def env_fn():
@@ -10,13 +10,13 @@ def run_experiment(args):
 
     eg = ExperimentGrid(name=args.exp_name)
     eg.add('env_fn', env_fn)
-    eg.add('seed', [10*i for i in range(args.num_runs)])
-    eg.add('epochs', 500)
-    eg.add('steps_per_epoch', 10000)
-    eg.add('save_freq', 20)
-    eg.add('max_ep_len', 200)
-    eg.add('ac_kwargs:activation', tf.tanh, '')
-    eg.run(ppo_tf1)
+    eg.add('seed', [i for i in range(args.num_runs)])
+    eg.add('epochs', args.epochs)
+    eg.add('steps_per_epoch', args.steps_per_epoch)
+    eg.add('save_freq', args.save_freq)
+    eg.add('max_ep_len', args.max_ep_len)
+    eg.add('ac_kwargs:activation', torch.nn.Tanh, '')
+    eg.run(ppo_pytorch, data_dir=args.data_dir, num_cpu=args.cpu)
 
 if __name__ == '__main__':
     import argparse
@@ -25,6 +25,11 @@ if __name__ == '__main__':
     parser.add_argument('--num_runs', type=int, default=5)
     parser.add_argument('--env_name', type=str, default="HumanoidRL-v0")
     parser.add_argument('--exp_name', type=str, default='ddpg-custom')
+    parser.add_argument('--data_dir', type=str, default='results')
+    parser.add_argument('--epochs', type=int, default=100000)
+    parser.add_argument('--steps_per_epoch', type=int, default=10000)
+    parser.add_argument('--max_ep_len', tpe=int, default=200)
+    parser.add_argument('--save_freq', tpe=int, default=20)
     args = parser.parse_args()
 
     run_experiment(args)
